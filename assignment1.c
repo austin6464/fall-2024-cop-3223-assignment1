7e7b9ad4ed08002ee343271d5e920d70410e33cf
austin6464 <au176464@ucf.edu>
2024-09-08 17:41:58 +0000

    first commit

--- assignment1.c
+++ assignment1.c
@@ -1,3 +1,23 @@
+#include <math.h>
+#include <stdio.h>
 
 
+// main function
 
+int main(void);
+{
+    calculatedistance();
+
+}
+
+// distance fuction
+
+double calculatedistance();
+{
+printf("enter the first point")
+scanf("%f%f", &x1 ,&y1);
+prinf("enter the second point");
+scanf(x2,y2);
+
+
+}
