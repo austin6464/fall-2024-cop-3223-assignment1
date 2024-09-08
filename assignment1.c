0f37fa731bb81dcdba2f244e84713da76f12071e
austin6464 <au176464@ucf.edu>
2024-09-08 20:07:09 +0000

    third commit

--- assignment1.c
+++ assignment1.c
@@ -1,19 +1,30 @@
 #include <math.h>
 #include <stdio.h>
 
-double distance(int x1,int x2, int y1, int y2, double distance)
-{
-printf("enter the first point");
-scanf(" %d%d", &x1 ,&y1);
-printf("enter the second point");
-scanf(" %d%d", &x2, &y2);
-// calculation
+// distance fuction for all calculations
 
-distance = sqrt(((x2 - x1)*(x2 - x1))+((y2 - y1)*(y2 - y1)));
-return 0;
+double distance(int x1, int x2, int y1, int y2){
+    double result = sqrt(((x2 - x1)*(x2 - x1))+((y2 - y1)*(y2 - y1)));
 }
+
+
+double calculatedistance()
+{
+    int x1 = 0;
+    int x2 = 0;
+    int y1 = 0;
+    int y2 = 0;
+    printf("enter the first point (x,y).\n");
+    scanf(" %d%d", &x1 ,&y1);
+    printf("enter the second point (x,y).\n");
+    scanf(" %d%d", &x2, &y2);
+    double the_distance = distance(x1,x2,y1,y2);
+    printf("the distance is %lf \n", the_distance);
+}
+
 // main function
 
-int main(int argc, char** argv) {
-    int calculatedistance();
+int main(int argc, char **argv)
+{
+    calculatedistance();
 }
